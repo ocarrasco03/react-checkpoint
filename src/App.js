fd78b7993e1bc6b508fcf933c2e0d0e2b08597d5
Oscar Carrasco <anubis.carrasco03@gmail.com>
2019-07-04 01:00:57 -0500

    Some configs for the checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require('dotenv').config();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: "",
+      limit: 20,
+      result: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://api.giphy.com/v1/gifs/search?q=${this.state.q}&api_key=${process.env.API_KEY}`)
+      .then((response) => {
+        console.log(response);
+        this.setState({ result: response.data });
+      })
+      .catch(e => console.log(`Unable to fetch data: ${e}`));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={8}>
+            {/* Search */}
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
